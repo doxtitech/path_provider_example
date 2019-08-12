9d6dab77eb9b838ad15a119d53c5ebbb73e1efa0
Ciprian Turcu <doxtitech@gmail.com>
2019-08-12 10:56:28 +0300

    finished demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -48,7 +50,20 @@ class _MyHomePageState extends State<MyHomePage> {
   String textContent =
       'Press the button below to write the file or read from it';
 
-  void _buttonPressed() {}
+  void _buttonPressed() async {
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+
+    File ourFile = File('$appDocPath/greeting.txt');
+    ourFile.writeAsString('Hello, Flutter Buddy');
+    setState(() {
+      textContent = 'File was written';
+    });
+    print(appDocDir.listSync());
+  }
 
   @override
   Widget build(BuildContext context) {
